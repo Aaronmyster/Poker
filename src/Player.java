a3c4b755175d2dce5ea0f7b8c5900f6e94e3dfeb
Aaronmyster <aaronmyster@gmail.com>
2013-06-13 18:24:04 -0500

    Woops! Forgot to save Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,17 @@
 public class Player {
 	
-	public String name;	//name of the player
-	public int sit
+	public String name;		//name of the player
+	public int sit; 		//seat number of the player
+	public int stack;		//stack amount for the player
+	public int inStack;		//stack amount before delt
+
+	public Player(String p_name, int p_sit, int p_stack, int p_inStack){
+		name = p_name;
+		sit = p_sit; 		
+		stack = p_stack;
+		inStack = p_inStack;
+	}
+
 
 }
+
